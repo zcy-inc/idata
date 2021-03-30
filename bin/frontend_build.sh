5a8eb9e65e497da8c268d1b130fdfc8d20eef07c
百里 <dengjiajie@cai-inc.com>
2021-03-30 14:06:35 +0800

    chore: 构建镜像名称修改

--- bin/frontend_build.sh
+++ bin/frontend_build.sh
@@ -2,6 +2,6 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 cd ./front-end
-npm i
+npm i --registry=http://mirrors-front.cai-inc.com
 npm run build
-docker build -t docker-image.cai-inc.com/bigdata/idata-front-end .
+docker build -t docker-image.cai-inc.com/bigdata/idata-portal-front .
