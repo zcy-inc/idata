353fa358458858511cd806434f7dd8443eccd067
百里 <dengjiajie@cai-inc.com>
2021-03-31 15:19:15 +0800

    feat: logo修改

--- front-end/config/defaultSettings.ts
+++ front-end/config/defaultSettings.ts
@@ -12,9 +12,9 @@ const Settings: LayoutSettings & {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'IData',
+  title: '',
   pwa: false,
-  logo: 'https://sitecdn.zcycdn.com/f2e-assets/d64f1b62-87ab-47ea-8e14-2ab710914b1c.png',
+  logo: 'https://sitecdn.zcycdn.com/f2e-assets/8e7350f7-a0de-460b-b2ba-a861eab56f06.svg',
   iconfontUrl: '//at.alicdn.com/t/font_2310080_1i868rl2a12.js',
 };
 
