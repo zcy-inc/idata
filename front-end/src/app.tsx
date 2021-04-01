01cce19e1aa655c16635fc19988b215d257c7433
百里 <dengjiajie@cai-inc.com>
2021-04-01 17:20:00 +0800

    feat: 去掉request errorHandler

--- front-end/src/app.tsx
+++ front-end/src/app.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { PageLoading } from '@ant-design/pro-layout';
-import { notification } from 'antd';
 import type { Settings as LayoutSettings } from '@ant-design/pro-layout';
 import { history } from 'umi';
 import type { RequestConfig, RunTimeLayoutConfig } from 'umi';
 import type { CurrentUser } from '@/interfaces/user';
-import type { ResponseError } from 'umi-request';
+import type { Context } from 'umi-request';
 import { RightContent } from '@/components';
 import { skip2Login } from '@zcy-data/idata-utils';
 import { getSystemState } from '@/services/global';
@@ -66,51 +65,16 @@ export const layout: RunTimeLayoutConfig = ({ initialState }) => {
   };
 };
 
-const codeMessage = {
-  200: '服务器成功返回请求的数据。',
-  201: '新建或修改数据成功。',
-  202: '一个请求已经进入后台排队（异步任务）。',
-  204: '删除数据成功。',
-  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-  401: '用户没有权限（令牌、用户名、密码错误）。',
-  403: '用户得到授权，但是访问是被禁止的。',
-  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
-  405: '请求方法不被允许。',
-  406: '请求的格式不可得。',
-  410: '请求的资源被永久删除，且不会再得到的。',
-  422: '当创建一个对象时，发生一个验证错误。',
-  500: '服务器发生错误，请检查服务器。',
-  502: '网关错误。',
-  503: '服务不可用，服务器暂时过载或维护。',
-  504: '网关超时。',
-};
-
-/**
- * 异常处理程序
- */
-const errorHandler = (error: ResponseError) => {
-  const { response, data } = error;
-  if (response && response.status) {
-    const errorText = codeMessage[response.status] || response.statusText;
-    const { status, url } = response;
-
-    notification.error({
-      message: `请求错误 ${status}: ${url}`,
-      description: errorText,
-    });
+const middleware = async (ctx: Context, next: () => void) => {
+  await next();
+  const { code } = ctx.res;
+  if (code === '401') {
+    skip2Login({ redirect: true, redirectUrl: window.location.href });
+    return;
   }
-
-  if (!response) {
-    notification.error({
-      description: data.msg,
-      message: '出错啦！',
-    });
-  }
-  throw error;
 };
 
 export const request: RequestConfig = {
-  errorHandler,
   errorConfig: {
     adaptor: (resData) => {
       return {
@@ -120,4 +84,5 @@ export const request: RequestConfig = {
       };
     },
   },
+  middlewares: [middleware],
 };
