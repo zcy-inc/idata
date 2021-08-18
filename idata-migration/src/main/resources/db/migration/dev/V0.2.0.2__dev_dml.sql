2294875a21bdf18ffb9d34b20460d7302cb6f525
时光 <caizhedong@cai-inc.com>
2021-08-18 11:36:05 +0800

    数仓设计模块sql脚本修改

--- idata-migration/src/main/resources/db/migration/dev/V0.2.0.2__dev_dml.sql
+++ idata-migration/src/main/resources/db/migration/dev/V0.2.0.2__dev_dml.sql
@@ -1,12 +1,26 @@
 -- ### 新表预置数据：
 -- dev_folder
 -- 系统文件夹
-insert into idata.dev_folder (id, creator, folder_name) values (1, '系统管理员', 'SystemFolder');
-insert into idata.dev_folder (id, creator, folder_name, parent_id) values (2, '系统管理员', 'LabelSystemFolder', 1);
-insert into idata.dev_folder (id, creator, folder_name, parent_id) values (3, '系统管理员', 'EnumSystemFolder', 1);
+insert into idata.dev_folder (id, creator, folder_name, folder_type) values (1, '系统管理员', 'LabelSystemFolder', 'LABEL');
+insert into idata.dev_folder (id, creator, folder_name, folder_type) values (2, '系统管理员', 'EnumSystemFolder', 'ENUM');
+
+insert into idata.dev_folder (id, creator, folder_name, folder_type) values (3, '系统管理员', 'ODS', 'TABLE');
+insert into idata.dev_folder (id, creator, folder_name, folder_type) values (4, '系统管理员', 'DWD', 'TABLE');
+insert into idata.dev_folder (id, creator, folder_name, folder_type) values (5, '系统管理员', 'DWS', 'TABLE');
+insert into idata.dev_folder (id, creator, folder_name, folder_type) values (6, '系统管理员', 'DIM', 'TABLE');
+insert into idata.dev_folder (id, creator, folder_name, folder_type) values (7, '系统管理员', 'ADS', 'TABLE');
+
+insert into idata.dev_folder (id, creator, folder_name, folder_type) values (8, '系统管理员', 'DimensionFolder',
+'DIMENSION');
+insert into idata.dev_folder (id, creator, folder_name, folder_type) values (9, '系统管理员', 'ModifierFolder', 'MODIFIER');
+insert into idata.dev_folder (id, creator, folder_name, folder_type) values (10, '系统管理员', 'MetricFolder', 'METRIC');
+insert into idata.dev_folder (id, creator, folder_name, parent_id, folder_type) values (11, '系统管理员', 'AtomicMetricFolder', 10, 'METRIC');
+insert into idata.dev_folder (id, creator, folder_name, parent_id, folder_type) values (12, '系统管理员', 'DeriveMetricFolder', 10, 'METRIC');
+insert into idata.dev_folder (id, creator, folder_name, parent_id, folder_type) values (13, '系统管理员', 'ComplexMetricFolder', 10, 'METRIC');
+
 --
 -- dev_label_define
--- 表：数据库名称、是否分区表、安全等级、数仓分层、表中文名称、数仓所属人、业务方所属人
+-- 表：数据库名称、是否分区表、安全等级、数仓分层、表中文名称、数仓所属人、业务方所属人、数据资产、数据域、metabase地址、描述、业务过程
 insert into idata.dev_label_define (creator, label_code, label_name, label_tag, label_param_type, subject_type,
 label_required, folder_id)
 values ('系统管理员', 'dbName:LABEL', '数据库名称', 'STRING_LABEL', 'STRING', 'TABLE', 1, 2);
@@ -35,7 +49,27 @@ insert into idata.dev_label_define (creator, label_code, label_name, label_tag, 
 label_required, folder_id)
 values ('系统管理员', 'pwOwnerId:LABEL', '业务所属人', 'STRING_LABEL', 'STRING', 'TABLE', 1, 2);
 
--- 字段：主键、字段类型、安全等级、字段中文名称、是否分区字段
+insert into idata.dev_label_define (creator, label_code, label_name, label_tag, label_param_type, subject_type,
+label_required, folder_id)
+values ('系统管理员', 'assetCatalogue:LABEL', '数据资产', 'ENUM_VALUE_LABEL', 'assetCatalogueEnum:ENUM', 'TABLE', 0, 2);
+
+insert into idata.dev_label_define (creator, label_code, label_name, label_tag, label_param_type, subject_type,
+label_required, folder_id)
+values ('系统管理员', 'domainId:LABEL', '数据域', 'ENUM_VALUE_LABEL', 'domainIdEnum:ENUM', 'TABLE', 1, 2);
+
+insert into idata.dev_label_define (creator, label_code, label_name, label_tag, label_param_type, subject_type,
+label_required, folder_id)
+values ('系统管理员', 'metabaseUrl:LABEL', 'Metabase地址', 'STRING_LABEL', 'STRING', 'TABLE', 0, 2);
+
+insert into idata.dev_label_define (creator, label_code, label_name, label_tag, label_param_type, subject_type,
+label_required, folder_id)
+values ('系统管理员', 'tblDescription:LABEL', '描述', 'STRING_LABEL', 'STRING', 'TABLE', 0, 2);
+
+insert into idata.dev_label_define (creator, label_code, label_name, label_tag, label_param_type, subject_type,
+label_required, folder_id)
+values ('系统管理员', 'bizProcess:LABEL', '业务过程', 'ENUM_VALUE_LABEL', 'bizProcessEnum:ENUM', 'TABLE', 0, 2);
+
+-- 字段：主键、字段类型、安全等级、字段中文名称、是否分区字段、描述
 insert into idata.dev_label_define (creator, label_code, label_name, label_tag, label_param_type, subject_type,
 label_required, folder_id)
 values ('系统管理员', 'pk:LABEL', '是否主键', 'BOOLEAN_LABEL', 'BOOLEAN', 'COLUMN', 1, 2);
@@ -55,31 +89,35 @@ values ('系统管理员', 'columnComment:LABEL', '字段中文名称', 'STRING_
 insert into idata.dev_label_define (creator, label_code, label_name, label_tag, label_param_type, subject_type,
 label_required, folder_id)
 values ('系统管理员', 'partitionedCol:LABEL', '是否分区字段', 'BOOLEAN_LABEL', 'BOOLEAN', 'COLUMN', 1, 2);
+
+insert into idata.dev_label_define (creator, label_code, label_name, label_tag, label_param_type, subject_type,
+label_required, folder_id)
+values ('系统管理员', 'columnDescription:LABEL', '描述', 'STRING_LABEL', 'STRING', 'COLUMN', 0, 2);
 --
 -- dev_enum、dev_enum_value
--- 数据域、业务过程、数仓分层、安全等级、hive字段类型、聚合方式
+-- 数据域、业务过程、数仓分层、安全等级、hive字段类型、聚合方式、数据资产
 -- 数据域
-insert into idata.dev_enum (creator, enum_code, enum_name, folder_id) values ('系统管理员', 'domainNameEnum:ENUM', '数据域', 3);
+insert into idata.dev_enum (creator, enum_code, enum_name, folder_id) values ('系统管理员', 'domainIdEnum:ENUM', '数据域', 3);
 insert into idata.dev_enum_value (creator, enum_code, value_code, enum_value)
-values ('系统管理员', 'domainNameEnum:ENUM', 'USER_DOMAIN:ENUM_VALUE', '用户域');
+values ('系统管理员', 'domainIdEnum:ENUM', 'USER_DOMAIN:ENUM_VALUE', '用户域');
 insert into idata.dev_enum_value (creator, enum_code, value_code, enum_value)
-values ('系统管理员', 'domainNameEnum:ENUM', 'TRADE_DOMAIN:ENUM_VALUE', '交易域');
+values ('系统管理员', 'domainIdEnum:ENUM', 'TRADE_DOMAIN:ENUM_VALUE', '交易域');
 insert into idata.dev_enum_value (creator, enum_code, value_code, enum_value)
-values ('系统管理员', 'domainNameEnum:ENUM', 'PRODUCT_DOMAIN:ENUM_VALUE', '商品域');
+values ('系统管理员', 'domainIdEnum:ENUM', 'PRODUCT_DOMAIN:ENUM_VALUE', '商品域');
 insert into idata.dev_enum_value (creator, enum_code, value_code, enum_value)
-values ('系统管理员', 'domainNameEnum:ENUM', 'USER_BEHAVIOUR_DOMAIN:ENUM_VALUE', '用户行为域');
+values ('系统管理员', 'domainIdEnum:ENUM', 'USER_BEHAVIOUR_DOMAIN:ENUM_VALUE', '用户行为域');
 insert into idata.dev_enum_value (creator, enum_code, value_code, enum_value)
-values ('系统管理员', 'domainNameEnum:ENUM', 'FILE_DOMAIN:ENUM_VALUE', '文件域');
+values ('系统管理员', 'domainIdEnum:ENUM', 'FILE_DOMAIN:ENUM_VALUE', '文件域');
 insert into idata.dev_enum_value (creator, enum_code, value_code, enum_value)
-values ('系统管理员', 'domainNameEnum:ENUM', 'GENERAL_DOMAIN:ENUM_VALUE', '通用域');
+values ('系统管理员', 'domainIdEnum:ENUM', 'GENERAL_DOMAIN:ENUM_VALUE', '通用域');
 insert into idata.dev_enum_value (creator, enum_code, value_code, enum_value)
-values ('系统管理员', 'domainNameEnum:ENUM', 'PROCURE_DOMAIN:ENUM_VALUE', '采购域');
+values ('系统管理员', 'domainIdEnum:ENUM', 'PROCURE_DOMAIN:ENUM_VALUE', '采购域');
 insert into idata.dev_enum_value (creator, enum_code, value_code, enum_value)
-values ('系统管理员', 'domainNameEnum:ENUM', 'ECOLOGY_DOMAIN:ENUM_VALUE', '金融域');
+values ('系统管理员', 'domainIdEnum:ENUM', 'ECOLOGY_DOMAIN:ENUM_VALUE', '金融域');
 insert into idata.dev_enum_value (creator, enum_code, value_code, enum_value)
-values ('系统管理员', 'domainNameEnum:ENUM', 'FINANCE_DOMAIN:ENUM_VALUE', '财务域');
+values ('系统管理员', 'domainIdEnum:ENUM', 'FINANCE_DOMAIN:ENUM_VALUE', '财务域');
 
-insert into idata.dev_enum (creator, enum_code, enum_name, folder_id) values ('系统管理员', 'bizTypeEnum:ENUM', '业务过程', 3);
+insert into idata.dev_enum (creator, enum_code, enum_name, folder_id) values ('系统管理员', 'bizProcessEnum:ENUM', '业务过程', 3);
 
 -- 数仓分层
 insert into idata.dev_enum (creator, enum_code, enum_name, folder_id) values ('系统管理员', 'dwLayerEnum:ENUM', '数仓分层', 3);
@@ -123,6 +161,13 @@ values ('系统管理员', 'securityLevelEnum:ENUM', 'SECURITY_LEVEL_B3:ENUM_VAL
 insert into idata.dev_enum_value (creator, enum_code, value_code, enum_value)
 values ('系统管理员', 'securityLevelEnum:ENUM', 'SECURITY_LEVEL_B4:ENUM_VALUE', '政采云机密数据(B4)');
 
+insert into idata.dev_enum_value (creator, enum_code, value_code, enum_value)
+values ('系统管理员', 'securityLevelEnum:ENUM', 'SECURITY_LEVEL_LOW:ENUM_VALUE', '低');
+insert into idata.dev_enum_value (creator, enum_code, value_code, enum_value)
+values ('系统管理员', 'securityLevelEnum:ENUM', 'SECURITY_LEVEL_MEDIUM:ENUM_VALUE', '中');
+insert into idata.dev_enum_value (creator, enum_code, value_code, enum_value)
+values ('系统管理员', 'securityLevelEnum:ENUM', 'SECURITY_LEVEL_HIGH:ENUM_VALUE', '高');
+
 -- hive字段类型
 insert into idata.dev_enum (creator, enum_code, enum_name, folder_id) values ('系统管理员', 'hiveColTypeEnum:ENUM', 'hive字段类型', 3);
 insert into idata.dev_enum_value (creator, enum_code, value_code, enum_value)
@@ -161,6 +206,7 @@ values ('系统管理员', 'aggregatorEnum:ENUM', 'AGGREGATOR_CNT:ENUM_VALUE', '
 insert into idata.dev_enum_value (creator, enum_code, value_code, enum_value)
 values ('系统管理员', 'aggregatorEnum:ENUM', 'AGGREGATOR_CNTD:ENUM_VALUE', '去重计数');
 
+insert into idata.dev_enum (creator, enum_code, enum_name, folder_id) values ('系统管理员', 'assetCatalogueEnum:ENUM', '数据资产', 3);
 --
 -- dev_enum_value
 -- 同dev_enum
