5d6088db179faafe42226f188f2bc907904f4264
大海 <chenkaixia@cai-inc.com>
2021-11-08 10:27:00 +0800

    feat:完成抽数配置和livy

--- front-end/src/pages/Integrated/Components/Scheduling.tsx
+++ front-end/src/pages/Integrated/Components/Scheduling.tsx
@@ -1,15 +1,25 @@
+import type { FC } from 'react';
 import React, { useState, useRef } from 'react';
 import type { ProColumns, ActionType } from '@ant-design/pro-table';
 import { EditableProTable } from '@ant-design/pro-table';
 import { PoweroffOutlined } from '@ant-design/icons';
-import { Button,Alert,Space } from 'antd';
+import { Button, Alert, Space } from 'antd';
+import props from '../../../.umi/plugin-layout/Layout';
 type DataSourceType = {
   id: React.Key;
   configValue?: string;
   configValueKey?: string;
   configValueRemarks?: string;
 };
-
+interface IConnection {
+  url: string;
+  token?: string;
+  username?: string;
+  password?: string
+}
+interface IBaseConfiguration {
+  connection?: IConnection
+}
 const defaultData: DataSourceType[] = new Array(3).fill(1).map((_, index) => {
   return {
     id: (Date.now() + index).toString(),
@@ -18,66 +28,67 @@ const defaultData: DataSourceType[] = new Array(3).fill(1).map((_, index) => {
     configValueRemarks: 'open',
   };
 });
-
-export default () => {
+const columns: ProColumns<DataSourceType>[] = [
+  {
+    title: '参数名称',
+    dataIndex: 'configValueKey',
+    width: '100px',
+    editable: false
+  },
+  {
+    title: '值',
+    dataIndex: 'configValue',
+    width: '280px',
+  },
+  {
+    title: '备注',
+    dataIndex: 'configValueRemarks',
+  },
+];
+const BaseConfiguration: FC<IBaseConfiguration> = (props) => {
+  const { connection } = props;
   const [editableKeys, setEditableRowKeys] = useState<React.Key[]>(() =>
     defaultData.map((item) => item.id),
   );
   const actionRef = useRef<ActionType>();
   const [dataSource, setDataSource] = useState<DataSourceType[]>(() => defaultData);
 
-  const columns: ProColumns<DataSourceType>[] = [
-    {
-      title: '参数名称',
-      dataIndex: 'configValueKey',
-      width: '100px',
-      editable: false
-    },
-    {
-      title: '值',
-      dataIndex: 'configValue',
-      width: '280px',
-    },
-    {
-      title: '备注',
-      dataIndex: 'configValueRemarks',
-    },
-  ];
-
   return (
     <>
-        <EditableProTable<DataSourceType>
-          columns={columns}
-          rowKey="id"
-          value={dataSource}
-          actionRef={actionRef}
-          onChange={setDataSource}
-          recordCreatorProps={false}
-          editable={{
-            type: 'multiple',
-            editableKeys,
-            onValuesChange: (record, recordList) => {
-              setDataSource(recordList);
-            },
-            onChange: setEditableRowKeys,
-          }}
-        />
-        <div>
+      <EditableProTable<DataSourceType>
+        columns={columns}
+        rowKey="id"
+        value={dataSource}
+        actionRef={actionRef}
+        onChange={setDataSource}
+        recordCreatorProps={false}
+        editable={{
+          type: 'multiple',
+          editableKeys,
+          onValuesChange: (record, recordList) => {
+            setDataSource(recordList);
+          },
+          onChange: setEditableRowKeys,
+        }}
+      />
+      {connection ?
         <Space>
-        <Button
+          <Button
             type="primary"
             size="small"
             icon={<PoweroffOutlined />}
           >
-              点击测试联调性
+            点击测试联调性
           </Button>
           <Alert message="连接成功" type="success" showIcon />
           <Alert message="连接失败" type="error" showIcon />
-          </Space>
-        </div>
-        <div style={{textAlign: 'right'}}>
-          <Button type="primary"> 保存</Button>
-        </div>
+        </Space>
+        : null}
+
+      <div style={{ textAlign: 'right' }}>
+        <Button type="primary"> 保存</Button>
+      </div>
     </>
   );
 };
+export default BaseConfiguration
--- front-end/src/pages/Integrated/index.tsx
+++ front-end/src/pages/Integrated/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
 import { Tabs } from 'antd';
 import Scheduling from './Components/Scheduling';
-import Hadoop  from './Components/Hadoop';
+import Hadoop from './Components/Hadoop';
 import "./index.less";
 const { TabPane } = Tabs;
 export default () => (
@@ -27,13 +27,19 @@ export default () => (
     <div className="zcy-content" >
       <Tabs defaultActiveKey="1">
         <TabPane tab="调度系统" key="1">
-          <Scheduling/>
+          <Scheduling connection={{url:""}} />
         </TabPane>
         <TabPane tab="Hive MetaStore" key="2">
-          <Scheduling />
+        <Scheduling connection={{url:""}} />
         </TabPane>
         <TabPane tab="Hadoop" key="3">
           <Hadoop />
+        </TabPane>
+        <TabPane tab="抽数配置" key="4">
+          <Scheduling />
+        </TabPane>
+        <TabPane tab="livy" key="5">
+          <Scheduling />
         </TabPane>
       </Tabs>
     </div>
