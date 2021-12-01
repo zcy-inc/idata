5703f91d3d3655b53efe14621a05f3b8a0a8b8a7
北笙 <zhanqian@cai-inc.com>
2021-12-01 10:01:57 +0800

    feature: record sql

--- idata-develop/src/main/resources/sql/V0.4.3_change.sql
+++ idata-develop/src/main/resources/sql/V0.4.3_change.sql
@@ -1,6 +1,9 @@
+alter table dev_table_info
+    add hive_table_name varchar(200) null comment '同步至hive的表' after table_name;
+
+
 alter table dev_label
     add hidden tinyint default 0 null comment '是否隐藏不展示';
-
 
 
 INSERT INTO idata.dev_label_define (del, creator, create_time, editor, edit_time, label_code, label_name, label_tag,
