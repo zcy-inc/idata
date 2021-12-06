f109be7f7c4c50ef068b0fb8df6fe61626a00989
时光 <caizhedong@cai-inc.com>
2021-12-06 10:42:44 +0800

    脚本注释

--- idata-migration/src/main/resources/db/migration/dev/V0.4.3.1__dev_ddl.sql
+++ idata-migration/src/main/resources/db/migration/dev/V0.4.3.1__dev_ddl.sql
@@ -1,8 +1,8 @@
-alter table dev_table_info
-    add hive_table_name varchar(200) null comment '同步至hive的表' after table_name;
-
-
-alter table dev_label
-    add hidden tinyint default 0 null comment '是否隐藏不展示';
+-- alter table dev_table_info
+--     add hive_table_name varchar(200) null comment '同步至hive的表' after table_name;
+--
+--
+-- alter table dev_label
+--     add hidden tinyint default 0 null comment '是否隐藏不展示';
 
 
