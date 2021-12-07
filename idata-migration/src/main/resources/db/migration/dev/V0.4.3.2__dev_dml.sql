2a6f41d49a3117c2f49ffb450ce941cb9b546d27
时光 <caizhedong@cai-inc.com>
2021-12-07 19:10:58 +0800

    数据库脚本修改

--- idata-migration/src/main/resources/db/migration/dev/V0.4.3.2__dev_dml.sql
+++ idata-migration/src/main/resources/db/migration/dev/V0.4.3.2__dev_dml.sql
@@ -1,28 +1,22 @@
--- INSERT INTO idata.dev_label_define (del, creator, create_time, editor, edit_time, label_code, label_name, label_tag,
---                                     label_param_type, label_attributes, special_attribute, subject_type, label_index,
---                                     label_required, label_scope, folder_id)
--- VALUES (1, '系统管理员', '2021-08-18 22:30:35.267', '', '2021-11-26 13:55:13.704', 'hiveColumnComment:LABEL',
---         '同步至hive的注释', 'STRING_LABEL', 'STRING', '[]', '{}', 'COLUMN', null, 0, null, 0);
---
--- INSERT INTO idata.dev_label_define (del, creator, create_time, editor, edit_time, label_code, label_name, label_tag,
---                                     label_param_type, label_attributes, special_attribute, subject_type, label_index,
---                                     label_required, label_scope, folder_id)
--- VALUES (1, '系统管理员', '2021-08-18 22:30:35.267', '', '2021-11-26 13:55:13.704', 'hiveColumnComment:LABEL',
---         '同步至hive的注释', 'STRING_LABEL', 'STRING', '[]', '{}', 'COLUMN', null, 0, null, 0);
---
--- INSERT INTO idata.dev_label_define (del, creator, create_time, editor, edit_time, label_code, label_name, label_tag,
---                                     label_param_type, label_attributes, special_attribute, subject_type, label_index,
---                                     label_required, label_scope, folder_id)
--- VALUES (1, '系统管理员', '2021-08-18 22:30:35.267', '', '2021-11-26 13:55:13.396', 'hiveColumnType:LABEL',
---         '同步至hive的类型', 'STRING_LABEL', 'STRING', '[]', '{}', 'COLUMN', null, 0, null, 0);
---
--- INSERT INTO idata.dev_label_define (del, creator, create_time, editor, edit_time, label_code, label_name, label_tag,
---                                     label_param_type, label_attributes, special_attribute, subject_type, label_index,
---                                     label_required, label_scope, folder_id)
--- VALUES (1, '系统管理员', '2021-08-18 22:30:35.267', '', '2021-11-26 13:55:13.555', 'hiveColumnName:LABEL',
---         '同步至hive的名称', 'STRING_LABEL', 'STRING', '[]', '{}', 'COLUMN', null, 0, null, 0);
+INSERT INTO idata.dev_label_define (del, creator, create_time, editor, edit_time, label_code, label_name, label_tag,
+                                    label_param_type, label_attributes, special_attribute, subject_type, label_index,
+                                    label_required, label_scope, folder_id)
+VALUES (1, '系统管理员', '2021-08-18 22:30:35.267', '', '2021-11-26 13:55:13.704', 'hiveColumnComment:LABEL',
+        '同步至hive的注释', 'STRING_LABEL', 'STRING', '[]', '{}', 'COLUMN', null, 0, null, 0);
+
+INSERT INTO idata.dev_label_define (del, creator, create_time, editor, edit_time, label_code, label_name, label_tag,
+                                    label_param_type, label_attributes, special_attribute, subject_type, label_index,
+                                    label_required, label_scope, folder_id)
+VALUES (1, '系统管理员', '2021-08-18 22:30:35.267', '', '2021-11-26 13:55:13.396', 'hiveColumnType:LABEL',
+        '同步至hive的类型', 'STRING_LABEL', 'STRING', '[]', '{}', 'COLUMN', null, 0, null, 0);
+
+INSERT INTO idata.dev_label_define (del, creator, create_time, editor, edit_time, label_code, label_name, label_tag,
+                                    label_param_type, label_attributes, special_attribute, subject_type, label_index,
+                                    label_required, label_scope, folder_id)
+VALUES (1, '系统管理员', '2021-08-18 22:30:35.267', '', '2021-11-26 13:55:13.555', 'hiveColumnName:LABEL',
+        '同步至hive的名称', 'STRING_LABEL', 'STRING', '[]', '{}', 'COLUMN', null, 0, null, 0);
 
 
--- update dev_label t1 , dev_column_info t2
--- set t1.column_id = t2.id
--- where t1.table_id = t2.table_id and t1.column_name = t2.column_name
+update dev_label t1 , dev_column_info t2
+set t1.column_id = t2.id
+where t1.table_id = t2.table_id and t1.column_name = t2.column_name and t1.column_id is not null;
