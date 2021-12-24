6aeb2facc120fc9a1b4132a4cddc7dad6fb32975
大海 <chenkaixia@cai-inc.com>
2021-12-24 13:37:12 +0800

    fix:代码冲突合并

--- front-end/src/pages/datapi/DataDev/components/TabDev/components/Content/SparkSql/index.tsx
+++ front-end/src/pages/datapi/DataDev/components/TabDev/components/Content/SparkSql/index.tsx
@@ -5,13 +5,9 @@ import { Form, Input, Modal, Select, Table, Tabs } from 'antd';
 import type { ForwardRefRenderFunction } from 'react';
 import type { IDisposable } from 'monaco-editor';
 import styles from './index.less';
-<<<<<<< HEAD
 import { getUDFList } from '@/services/datadev';
-import { UDF } from '@/types/datadev';
+import type { UDF } from '@/types/datadev';
 
-=======
-import SqlEditor from '@/components/SqlEditor';
->>>>>>> feat:新增自动联动逻辑
 interface SparkSqlProps {
   monaco: any;
   data: {
@@ -75,7 +71,6 @@ const SparkSql: ForwardRefRenderFunction<unknown, SparkSqlProps> = (
 
   return (
     <>
-<<<<<<< HEAD
       <div style={{ position: 'relative', height: monacoHeight }}>
         <SplitPane split="horizontal" defaultSize="60%">
           <MonacoEditor
@@ -149,55 +144,6 @@ const SparkSql: ForwardRefRenderFunction<unknown, SparkSqlProps> = (
                 value: `${_.id}`,
               }))}
               style={{ width }}
-=======
-      <Tabs>
-        <TabPane tab="编辑器" key="editor" style={{ padding: '16px 0', height: 440 }} forceRender >
-          <SqlEditor />
-          </TabPane>
-        <TabPane tab="作业配置" key="config" style={{ padding: 16 }} forceRender>
-          <Form form={form} colon={false}>
-            <Item name="externalTables" label="外部表">
-              <Input placeholder="请输入" style={{ width }} />
-            </Item>
-            <Item name="udfIds" label="自定义函数">
-              <Select placeholder="请选择" options={[]} style={{ width }} />
-            </Item>
-          </Form>
-        </TabPane>
-      </Tabs>
-      <Tabs
-        className={styles.tabs}
-        type="editable-card"
-        hideAdd
-        onEdit={(key, action) => {
-          if (action === 'remove') {
-            const index = Number(key);
-            if (Number.isInteger(index)) {
-              removeResult(index);
-            }
-          }
-        }}
-      >
-        <TabPane tab="日志" key="log" style={{ height: 440, padding: '16px 0' }} closable={false}>
-          <MonacoEditor
-            height="400"
-            language="json"
-            theme="vs-dark"
-            value={log.join('\n')}
-            options={{ readOnly: true, automaticLayout: true }}
-          />
-        </TabPane>
-        {res.map((_, i) => (
-          <TabPane tab={`结果${i + 1}`} key={i} style={{ height: 440, padding: '16px 0' }}>
-            <Table
-              columns={_.columns}
-              dataSource={_.dataSource}
-              size="small"
-              pagination={{
-                hideOnSinglePage: true,
-                pageSize: 9999,
-              }}
->>>>>>> feat:新增自动联动逻辑
             />
           </Item>
         </Form>
