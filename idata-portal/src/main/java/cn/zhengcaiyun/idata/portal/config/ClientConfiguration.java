420cd2d42006db590bc7c39887ea07c7f56179ed
北笙 <zhanqian@cai-inc.com>
2022-01-26 16:10:44 +0800

    补充merge 提交

--- idata-portal/src/main/java/cn/zhengcaiyun/idata/portal/config/ClientConfiguration.java
+++ idata-portal/src/main/java/cn/zhengcaiyun/idata/portal/config/ClientConfiguration.java
@@ -2,8 +2,10 @@ package cn.zhengcaiyun.idata.portal.config;
 
 import cn.zhengcaiyun.idata.connector.clients.hive.ConnectInfo;
 import cn.zhengcaiyun.idata.connector.clients.hive.pool.HivePool;
+import cn.zhengcaiyun.idata.system.api.SystemConfigApi;
 import cn.zhengcaiyun.idata.system.dal.dao.SysConfigDao;
 import cn.zhengcaiyun.idata.system.dal.model.SysConfig;
+import cn.zhengcaiyun.idata.system.dto.ConfigDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,11 +22,13 @@ public class ClientConfiguration {
     @Autowired
     private SysConfigDao sysConfigDao;
 
+    @Autowired
+    private SystemConfigApi systemConfigApi;
+
     @Bean
     public HivePool getHivePool() {
-        SysConfig hiveConfig = sysConfigDao.selectOne(dsl -> dsl.where(sysConfig.keyOne, isEqualTo("hive-info"))).orElse(null);
-        checkState(Objects.nonNull(hiveConfig), "数据源连接信息不正确");
-        String jdbcUrl = hiveConfig.getValueOne();
+        ConfigDto systemConfigByKey = systemConfigApi.getSystemConfigByKey("hive-info");
+        String jdbcUrl = systemConfigByKey.getValueOne().get("hive-info").getConfigValue();
         ConnectInfo connectInfo = new ConnectInfo();
         connectInfo.setJdbc(jdbcUrl);
         return new HivePool(connectInfo);
