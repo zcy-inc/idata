536352b3377dac296fd32a0d82656f2bdb5f1708
熠然 <yangjianhua@cai-inc.com>
2022-02-07 14:56:49 +0800

    [migrate]修改迁移结果表字段长度

--- idata-migration/src/main/resources/db/migration/other/V1.0.0.12__migrate_ddl.sql
+++ idata-migration/src/main/resources/db/migration/other/V1.0.0.12__migrate_ddl.sql
@@ -5,7 +5,7 @@ create table if not exists oth_migrate_result
     create_time     datetime            not null default current_timestamp comment '创建时间',
     edit_time       datetime            not null default current_timestamp on update current_timestamp comment '修改时间',
     migrate_type    varchar(100)        NULL COMMENT '迁移操作类型',
-    reason          varchar(500)        NULL COMMENT '原因',
+    reason          text                NULL COMMENT '原因',
     data            mediumtext          NULL COMMENT '具体数据'
 ) engine = innodb
   auto_increment = 1
