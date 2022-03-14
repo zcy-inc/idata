4447b7b2a4cd12cbe70fc8c58b18f9781333a30e
北笙 <zhanqian@cai-inc.com>
2022-03-14 17:49:40 +0800

    change merge sql template

---
+++ idata-portal/src/main/resources/template/merge_sql_template2.sql
@@ -0,0 +1,24 @@
+#{#isMulPartition ? 'set hive.exec.dynamic.partition=true; set hive.exec.dynamic.partition.mode=nonstrict; set hive.exec.max.dynamic.partitions.pernode=1000;' : ''}
+alter table #{#tmpTable} drop if exists partition (pt<'${day-#{#days}d}');
+
+insert overwrite table #{#tmpTable} partition(pt='${day}')
+select #{#alisColumns} #{#isMulPartition ? ' ,t1.num' : ''}
+from
+(
+select #{#columns} #{#isMulPartition ? ' ,num' : ''}
+from #{#tmpTable} where pt='${day-1d}'
+) t1
+left join
+(
+select #{#columns} #{#isMulPartition ? ' ,num' : ''}
+from #{#destTable}
+) t2
+on #{#keyCondition} #{#isMulPartition ? 'and t1.num=t2.num' : ''};
+where #{#whereKeyConditionParam}
+union all
+select #{#columns}
+from #{#destTable};
+
+insert overwrite table #{#destTable} #{#isMulPartition ? 'partition(num)' : ''}
+select #{#columns} #{#isMulPartition ? ' ,num' : ''}
+from #{#tmpTable} where pt='${day}';
