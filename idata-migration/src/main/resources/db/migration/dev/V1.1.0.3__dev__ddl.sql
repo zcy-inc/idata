56618f27c030a2287d64d01ee991bd88beb641b9
时光 <caizhedong@cai-inc.com>
2022-03-18 10:56:24 +0800

    修改job_output表target_table_pk字段大小

--- idata-migration/src/main/resources/db/migration/dev/V1.1.0.3__dev__ddl.sql
+++ idata-migration/src/main/resources/db/migration/dev/V1.1.0.3__dev__ddl.sql
@@ -3,3 +3,4 @@ alter table dev_job_content_di
 
 alter table idata.dev_job_content_sql modify source_sql longtext comment '数据来源SQL';
 
+alter table idata.dev_job_output modify job_target_table_pk varchar(200) comment '数据来源表主键(写入模式为upsert时必填)';
