94827e9e399bcf4d81376adf6279b2a8c54a150f
立航 <leijianxiong@cai-inc.com>
2022-06-07 11:27:22 +0800

    fix: 测试问题修复

--- front-end/src/pages/datapi/DataDev/components/FolderTree/components/IconFilter/index.tsx
+++ front-end/src/pages/datapi/DataDev/components/FolderTree/components/IconFilter/index.tsx
@@ -2,13 +2,30 @@ import React, { useState } from 'react';
 import { IconFont } from '@/components';
 import type { FC } from 'react';
 
-const IconFilter: FC<{}> = ({}) => {
-  const [isHover, setIsHover] = useState(false);
+interface IconFilterProps {
+  onClick?: (e: any) => void;
+  onMouseEnter?: (e: any) => void;
+  onMouseLeave?: (e: any) => void;
+  onFocus?: (e: any) => void;
+}
 
+const IconFilter: FC<IconFilterProps> = ({ onClick, onFocus, onMouseEnter, onMouseLeave }) => {
+  const [isHover, setIsHover] = useState(false);
+  const hadnleMouseEnter = (e: any) => {
+    setIsHover(true);
+    onMouseEnter && onMouseEnter(e);
+  }
+
+  const handleMouseLeave = (e: any) => {
+    setIsHover(false);
+    onMouseLeave && onMouseLeave(e);
+  }
   return (
     <IconFont
-      onMouseEnter={() => setIsHover(true)}
-      onMouseLeave={() => setIsHover(false)}
+      onMouseEnter={hadnleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={onClick}
+      onFocus={onFocus}
       style={{ marginLeft: 8, cursor: 'pointer', lineHeight: 1,  fontSize: 18 }}
       type={isHover ? 'icon-shaixuan-copy' : 'icon-shaixuan'}
     />
