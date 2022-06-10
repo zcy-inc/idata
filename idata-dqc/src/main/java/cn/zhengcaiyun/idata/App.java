917d183f8cec1a9edfe418af0b5e309459858176
北笙 <zhanqian@cai-inc.com>
2022-06-10 17:32:32 +0800

    数据质量中心 模块

---
+++ idata-dqc/src/main/java/cn/zhengcaiyun/idata/App.java
@@ -0,0 +1,13 @@
+package cn.zhengcaiyun.idata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
