1ba00b049c2a8136d05d35736483ae68f652ae1b
鹿竹 <cuiyanli@cai-inc.com>
2022-07-12 11:14:20 +0800

    feat:查看日志窗口放大

--- front-end/src/pages/datapi/DataDev/components/TabDev/components/MonacoModal/index.tsx
+++ front-end/src/pages/datapi/DataDev/components/TabDev/components/MonacoModal/index.tsx
@@ -3,7 +3,8 @@ import type { FC } from 'react';
 import MonacoEditor from 'react-monaco-editor';
 import { Modal } from 'antd';
 import { getDevLog } from '@/services/datadev';
-
+import './index.less';
+import { FullscreenOutlined, FullscreenExitOutlined } from '@ant-design/icons';
 interface MonacoModalProps {
   visible: boolean;
   logParam: {
@@ -17,6 +18,31 @@ interface MonacoModalProps {
 const MonacoModal: FC<MonacoModalProps> = ({ visible, logParam, onClose }) => {
   
   const [log, setLog] = useState('');
+  const [fullScreen, setFullScreen] = useState('normal');
+  const modalProp = {
+    full: {
+      bodyStyle: {
+        padding: 16,
+        height: "calc(100vh - 55px)",
+      },
+      style: {
+        maxWidth: "100vw",
+        top: 0,
+        paddingBottom: 0
+      },
+      width: "100vw", 
+    },
+    normal: {
+      bodyStyle: {
+        padding: 16,
+      },
+      width: 800, 
+    }
+  };
+  const monacoProp = {
+    full: '100vh',
+    normal: 400,
+  };
 
   useEffect(() => {
     getDevLogHandle();
@@ -35,21 +61,33 @@ const MonacoModal: FC<MonacoModalProps> = ({ visible, logParam, onClose }) => {
     }
   }
 
+  const resizeModal = () => {
+    fullScreen === 'full' && setFullScreen('normal');
+    fullScreen === 'normal' && setFullScreen('full');
+  }
+
   return <Modal
-    title="日志"
+    title="查看日志"
     visible={visible}
     onCancel={onClose}
     footer={null}
-    bodyStyle={{ padding: 16 }}
-    width={800}
+    {
+      ...modalProp[fullScreen]
+    }
     >
       <MonacoEditor
-        height="400"
+        height={monacoProp[fullScreen]}
         language="json"
         theme="vs-dark"
         value={log}
         options={{ readOnly: true }}
       />
+      {
+        fullScreen === 'normal' && <FullscreenOutlined className='log-monaco_resize' onClick={resizeModal}/>
+      }
+      {
+        fullScreen === 'full' && <FullscreenExitOutlined className='log-monaco_resize'  onClick={resizeModal}/>
+      }
   </Modal>
 };
 
