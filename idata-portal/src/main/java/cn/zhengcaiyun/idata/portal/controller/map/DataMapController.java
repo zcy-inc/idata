75acf9c4fba8bbbf826e0a7ca3e2f86dd54552b9
北笙 <zhanqian@cai-inc.com>
2022-09-29 15:41:18 +0800

    数据地图 收藏

--- idata-portal/src/main/java/cn/zhengcaiyun/idata/portal/controller/map/DataMapController.java
+++ idata-portal/src/main/java/cn/zhengcaiyun/idata/portal/controller/map/DataMapController.java
@@ -17,18 +17,30 @@
 
 package cn.zhengcaiyun.idata.portal.controller.map;
 
+import cn.hutool.core.collection.CollectionUtil;
+import cn.zhengcaiyun.idata.commons.context.OperatorContext;
+import cn.zhengcaiyun.idata.commons.enums.WhetherEnum;
 import cn.zhengcaiyun.idata.commons.pojo.Page;
 import cn.zhengcaiyun.idata.commons.pojo.PageParam;
 import cn.zhengcaiyun.idata.commons.pojo.RestResult;
+import cn.zhengcaiyun.idata.develop.dto.label.EnumValueDto;
+import cn.zhengcaiyun.idata.develop.util.MyBeanUtils;
 import cn.zhengcaiyun.idata.map.bean.condition.CategoryCond;
 import cn.zhengcaiyun.idata.map.bean.condition.DataSearchCond;
 import cn.zhengcaiyun.idata.map.bean.dto.CategoryTreeNodeDto;
 import cn.zhengcaiyun.idata.map.bean.dto.DataEntityDto;
+import cn.zhengcaiyun.idata.map.dal.dao.MapUserFavouriteDao;
+import cn.zhengcaiyun.idata.map.dal.model.MapUserFavourite;
 import cn.zhengcaiyun.idata.map.facade.DataMapFacade;
+import cn.zhengcaiyun.idata.portal.model.request.map.UserFavoriteRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static cn.zhengcaiyun.idata.develop.dal.dao.DevEnumValueDynamicSqlSupport.devEnumValue;
+import static cn.zhengcaiyun.idata.map.dal.dao.MapUserFavouriteDynamicSqlSupport.mapUserFavourite;
+import static org.mybatis.dynamic.sql.SqlBuilder.*;
 
 /**
  * data-map-controller
@@ -42,6 +54,9 @@ import java.util.List;
 public class DataMapController {
 
     private final DataMapFacade dataMapFacade;
+
+    @Autowired
+    private MapUserFavouriteDao mapUserFavouriteDao;
 
     @Autowired
     public DataMapController(DataMapFacade dataMapFacade) {
@@ -73,4 +88,48 @@ public class DataMapController {
     public RestResult<List<CategoryTreeNodeDto>> getCategory(CategoryCond condition) {
         return RestResult.success(dataMapFacade.getCategory(condition));
     }
+
+    /**
+     * 收藏表/指标
+     * @param request
+     * @return
+     */
+    @PostMapping("/addFavorite")
+    public RestResult<Boolean> addFavorite(@RequestBody UserFavoriteRequest request) {
+        MapUserFavourite userFavourite = MyBeanUtils.copy(request, MapUserFavourite.class);
+        userFavourite.setUserId(OperatorContext.getCurrentOperator().getId());
+        userFavourite.setCreator(OperatorContext.getCurrentOperator().getNickname());
+        userFavourite.setDel(WhetherEnum.NO.val);
+        mapUserFavouriteDao.insert(userFavourite);
+        return RestResult.success(true);
+    }
+
+    /**
+     * 取消收藏表/指标
+     * @param request
+     * @return
+     */
+    @PostMapping("/cancelFavorite")
+    public RestResult<Boolean> cancelFavorite(@RequestBody UserFavoriteRequest request) {
+        mapUserFavouriteDao.delete(dsl -> dsl.where(mapUserFavourite.entityCode, isEqualTo(request.getEntityCode()),
+                and(mapUserFavourite.entitySource, isEqualTo(request.getEntitySource())),
+                and(mapUserFavourite.userId, isEqualTo(OperatorContext.getCurrentOperator().getId()))));
+        return RestResult.success(true);
+    }
+
+    /**
+     * 查询表/指标是否已收藏
+     * @param request
+     * @return
+     */
+    @PostMapping("/isFavorite")
+    public RestResult<Boolean> isFavorite(@RequestBody UserFavoriteRequest request) {
+        List<MapUserFavourite> list = mapUserFavouriteDao.select(c ->
+                c.where(mapUserFavourite.entityCode, isEqualTo(request.getEntityCode()),
+                        and(mapUserFavourite.entitySource, isEqualTo(request.getEntitySource())),
+                        and(mapUserFavourite.userId, isEqualTo(OperatorContext.getCurrentOperator().getId())),
+                        and(mapUserFavourite.del, isEqualTo(0))));
+        return RestResult.success(CollectionUtil.isNotEmpty(list));
+    }
+
 }
