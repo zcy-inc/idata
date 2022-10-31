19c6e4735cf4578dbd18ce4b2587b826ac3eda3e
元宿 <zhengxiuxiu@cai-inc.com>
2022-10-31 16:19:50 +0800

    表血缘解析bugfix

--- idata-connector/src/main/java/cn/zhengcaiyun/idata/connector/util/SparkSqlHelper.java
+++ idata-connector/src/main/java/cn/zhengcaiyun/idata/connector/util/SparkSqlHelper.java
@@ -5,6 +5,7 @@ import cn.zhengcaiyun.idata.connector.parser.spark.SparkSqlParser;
 import cn.zhengcaiyun.idata.connector.util.model.TableSib;
 import cn.zhengcaiyun.idata.connector.util.model.UpperCaseCharStream;
 import cn.zhengcaiyun.idata.connector.util.parse.ParseErrorListener;
+import cn.zhengcaiyun.idata.connector.util.parse.ParseException;
 import cn.zhengcaiyun.idata.connector.util.parse.PostProcessor;
 import cn.zhengcaiyun.idata.connector.util.parse.SparkSqlAst;
 import org.antlr.v4.runtime.CharStreams;
@@ -38,7 +39,7 @@ public class SparkSqlHelper {
 
         try {
             return visitor.visit(parser.singleStatement());
-        } catch (ParseCancellationException e) {
+        } catch (ParseCancellationException | ParseException e) {
             tokenStream.seek(0);
             parser.reset();
 
