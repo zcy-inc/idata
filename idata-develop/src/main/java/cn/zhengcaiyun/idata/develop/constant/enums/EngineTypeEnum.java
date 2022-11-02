8716050a1067270454cc368e22853d1e9e91b67f
北笙 <zhanqian@cai-inc.com>
2022-11-02 16:50:52 +0800

    定时失效

--- idata-develop/src/main/java/cn/zhengcaiyun/idata/develop/constant/enums/EngineTypeEnum.java
+++ idata-develop/src/main/java/cn/zhengcaiyun/idata/develop/constant/enums/EngineTypeEnum.java
@@ -1,7 +1,5 @@
 package cn.zhengcaiyun.idata.develop.constant.enums;
 
 public enum EngineTypeEnum {
-    SPARK, SPARK3, SQOOP, KYLIN,
-//    DORIS,
-    FLINK, STARROCKS
+    SPARK, SPARK3, SQOOP, KYLIN, FLINK, STARROCKS
 }
